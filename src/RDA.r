f9df918ee2f10b99d89fd122a46c9facb071c6c2
niebo11 <zhangniebo@gmail.com>
2020-01-12 13:49:04 +0100

    corrected

--- src/RDA.r
+++ src/RDA.r
@@ -2,22 +2,27 @@ library(caret)
 library(klaR)
 set.seed(825)
 
-f <- file.choose()
-
-load(f)
+load("../data/LOLdatanormalized.Rdata")
 data <- data.normalized
 
-#N <- nrow(data) 
-#learn <- sample(1:N, round(2*N/3))
-#nlearn <- length(learn)
-#ntest <- N - nlearn
-#sizes <- seq(1,20,by=2)
-#decays <- 10^seq(-3, 0 , by = 0.01)
-trc <- trainControl(method = "rda", number = 10, repeats = 10,classProbs = TRUE)
-rdaFit <- train(winner ~ ., data = data,
-                method = "rda",
-                trControl = trc,
-                gamma = 0, lambda = 1,
-                tuneLength = 4,
-                metric = "ROC")
-rdaFit
+                
+N <- nrow(data) 
+learn <- sample(1:N, round(2*N/3))
+nlearn <- length(learn)
+ntest <- N - nlearn
+decays <- 10^seq(-3, 0 , by = 0.1)
+trc <- trainControl(method = "repeatedcv", number = 10, repeats = 5, verboseIter = TRUE)
+modelParameters <- train(winner~., data = data, subset=learn, method = 'rda', maxit = 500, trace = FALSE, tuneGrid = expand.grid(.gamma = decays, .lambda = decays), trControl = trc)
+
+save(modelParameters, file = "modelRDAParameters.regul")
+
+sink("../results/RDAresultsParameters.txt")
+
+gammaFinal <- modelParameters$bestTune[,1]
+lambdaFinal <- modelParameters$bestTune[,2]
+
+cat("Final gamma")
+cat(gammaFinal)
+cat("Final lambda")
+cat(lambdaFinal)
+
